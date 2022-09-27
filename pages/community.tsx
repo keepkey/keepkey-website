0668b079ac9ae0673ac975e424a2ed60f62b225a
Nathan Moore <n@nmoore.net>
2022-09-26 18:55:46 -0600

    add hero images for community, integration guide

--- pages/community.tsx
+++ pages/community.tsx
@@ -3,13 +3,16 @@ import Link from 'next/link'
 import supportedAppsImg from 'public/images/supported-applications/supported-apps-collage.png'
 import shapeShiftDemoImg from 'public/images/software/shapeshift-demo-mac-md.png'
 import styles from '../styles/pages/Onboarding.module.scss'
-import heroBgImage from 'public/images/heros/geomatric-black.jpg'
+import heroBgImage from 'public/images/heros/community-web.jpg'
 import SecurityWarning from "../components/SecurityWarning"
 export default function Onboarding() {
   return (
     <>
       <Hero />
-      <Main />
+      <div className="h-96">
+        <div className="my-80"></div>
+      </div>
+      {/* <Main /> */}
     </>
   )
 }
@@ -18,8 +21,8 @@ export default function Onboarding() {
 const Hero = () => {
   return(
     <>
-      <div className="relative z-0 py-80 bg-black">
-        <div className={styles.hero__img}>
+      <div className="relative z-0 pt-52 pb-36 bg-black">
+        <div>
           <Image
           src={heroBgImage}
           alt='keepkey wallet on desk'
@@ -32,17 +35,17 @@ const Hero = () => {
         </div>
         <div className="container relative grid xl:grid-cols-[650px_1fr] items-center z-10">
           <div>
-            <h1 className="text-6xl leading-tight tracking-wide font-bold text-white mb-4">Support for Leading Crypto Applications</h1>
-            <p className="text-2xl text-white mb-8 leading-relaxed tracking-wide">See below for a list of our recommendations.</p>
+            <h1 className="text-6xl leading-tight tracking-wide font-bold text-white mb-4">Community</h1>
+            {/* <p className="text-2xl text-white mb-8 leading-relaxed tracking-wide">See below for a list of our recommendations.</p> */}
 
           </div>
             <div className="absolute max-w-[650px] top-[-70px] right-[-30px]">
-              <Image
+              {/* <Image
                 alt="KeepKey wallet"
                 src={supportedAppsImg}
                 quality={100}
                         >
-              </Image>
+              </Image> */}
             </div>
         </div>
 
