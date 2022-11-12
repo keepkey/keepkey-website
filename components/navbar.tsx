6f7c7deea7fd644b9f49cac0ef3e2428884bb3e3
highlander <bithighlander@gmail.com>
2022-11-12 15:03:11 -0700

    remove SS

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -95,20 +95,20 @@ export default function Navbar() {
           </div>
         ))}
 
-        <div className="ml-auto flex items-center mt-2">
-          <div className="hidden sm:inline-block mr-8 lg:mr-0">
-            <Link href="https://app.shapeshift.com">
-              <a target="_blank" className="btn btn--round btn--blue ml-auto">Launch Shapeshift</a>
-            </Link>
-          </div>
+        {/*<div className="ml-auto flex items-center mt-2">*/}
+        {/*  <div className="hidden sm:inline-block mr-8 lg:mr-0">*/}
+        {/*    <Link href="https://app.shapeshift.com">*/}
+        {/*      <a target="_blank" className="btn btn--round btn--blue ml-auto">Launch Shapeshift</a>*/}
+        {/*    </Link>*/}
+        {/*  </div>*/}
 
-          <MobileNav
-            toggleMobileNav={toggleMobileNav}
-            isActive={isActive}
-            navLinks={navLinks}
-          />
+        {/*  <MobileNav*/}
+        {/*    toggleMobileNav={toggleMobileNav}*/}
+        {/*    isActive={isActive}*/}
+        {/*    navLinks={navLinks}*/}
+        {/*  />*/}
 
-        </div>
+        {/*</div>*/}
       </div>
       {/* <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg className="ml-1 w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button> */}
       {/* <div id="dropdownNavbar" className="hidden z-10 w-44 font-normal bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
