c6db0471d74c151f484d5da2ec54e06faf61b0d0
highlander <bithighlander@gmail.com>
2022-12-04 15:46:30 -0600

    debug build

--- pages/application.tsx
+++ pages/application.tsx
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import heroBgImage from 'public/images/heros/blue-lines.jpg'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import HeroSimple from '../components/hero-simple';
 const pageTitle = "Application Waltkthrough";
-import walletVertical from '../../assets/images/desktop/pin.png'
 
 export default function DesktopGuide() {
 
@@ -23,18 +21,9 @@ export default function DesktopGuide() {
 
 const Main = () => {
     return (
-
         <section className="container">
             <h2 className="text-center mb-16">About The KeepKey Desktop Application</h2>
             <text>
-                <Image
-                    src={walletVertical}
-                    alt='keepkey api'
-                    objectFit="cover"
-                    quality={100}
-                    objectPosition="center"
-                    priority={true}
-                ></Image>
             </text>
             <br/>
             <p>
