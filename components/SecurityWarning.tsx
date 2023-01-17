a69b7ea71433913c19b19a162181ce90094d38a9
Highlander <BitHighlander@users.noreply.github.com>
2023-01-17 15:03:49 -0700

    Update SecurityWarning.tsx

--- components/SecurityWarning.tsx
+++ components/SecurityWarning.tsx
@@ -14,7 +14,7 @@ export default function SecurityWarning() {
       </div>
       <div className="md:ml-8">
         <h4 className="font-medium">Security Warning</h4>
-        <p className="mt-2">The KeepKey Chrome app has been retired, and there are scam versions which can steal your money. Use your KeepKey ONLY with the ShapeShift web platform.</p>
+        <p className="mt-2">The KeepKey Chrome app has been retired, and there are scam versions which can steal your money.</p>
       </div>
     </div>
   )
