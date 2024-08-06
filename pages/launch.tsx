d926c79ad2079814b33dd570900c4736d14cd389
bithighlander <bithighlander@gmail.com>
2024-08-05 23:19:57 -0600

    add install

--- pages/launch.tsx
+++ pages/launch.tsx
@@ -1,13 +1,37 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import Head from 'next/head';
 import heroBgImage from 'public/images/heros/blue-lines.jpg';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import HeroSimple from '../components/hero-simple';
 import pin from 'public/images/desktop/Pin.png'
 import Image from "next/image";
+import axios from "axios";
+import Link from "next/link";
 const pageTitle = "Launch";
 
 export default function Launch() {
+    const [urlMacOS, seturlMacOS] = useState('')
+    const [urlWindows, seturlWindows] = useState('')
+    const [urlLinux, seturlLinux] = useState('')
+    //find latest release
+    let findLatestReleaseLinks = async function () {
+        try {
+            let resp = await axios({ method: 'GET', url: 'https://api.github.com/repos/keepkey/keepkey-desktop/releases/latest' })
+            console.log('findLatestReleaseLinks', resp.data)
+            let version = resp.data.tag_name
+            version = version.replace("v", "")
+            seturlMacOS("https://github.com/keepkey/keepkey-desktop/releases/download/v" + version + "/KeepKey-Desktop-" + version + "-arm64.dmg")
+            seturlWindows("https://github.com/keepkey/keepkey-desktop/releases/download/v" + version + "/KeepKey-Desktop-Setup-" + version + ".exe")
+            seturlLinux("https://github.com/keepkey/keepkey-desktop/releases/download/v" + version + "/KeepKey-Desktop-" + version + ".AppImage")
+
+        } catch (e) {
+            console.error(' e: ', e)
+        }
+    }
+
+    useEffect(() => {
+        findLatestReleaseLinks()
+    }, []);
 
     const launchKeepKey = () => {
         try {
@@ -34,6 +58,27 @@ export default function Launch() {
                 heroBgImg={heroBgImage}
                 pageTitle={pageTitle}
             />
+            <div className="container mx-auto flex flex-col items-center justify-center text-center py-12">
+                <div>
+                    <h1 className="text-4xl leading-tight tracking-wide lg:text-5xl lg:leading-tight 2xl:text-6xl 2xl:leading-tight font-bold text-white mb-4">
+                        Introducing the new KeepKey Client
+                    </h1>
+                    <p className="text-xl leading-relaxed lg:text-2xl lg:leading-relaxed text-white mb-8 tracking-wide">
+                        Follow the guide below to get started using your KeepKey.
+                    </p>
+                    <div className="flex flex-wrap justify-center">
+                        <Link href={urlMacOS}>
+                            <a className="btn btn-md btn--transparent mb-5 md:mr-5">Download for macOS</a>
+                        </Link>
+                        <Link href={urlWindows}>
+                            <a className="btn btn-md btn--transparent mb-5 md:mr-5">Download for Windows</a>
+                        </Link>
+                        <Link href={urlLinux}>
+                            <a className="btn btn-md btn--transparent mb-5">Download for Linux</a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
             <Main />
         </>
     );
@@ -49,7 +94,6 @@ const Main = () => {
                 quality={100}
             >
             </Image>
-            <h2 className="text-center mb-16">Launching KeepKey Desktop</h2>
         </section>
     );
 }
