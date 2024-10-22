7d27b49fa68582b35b6f93bb4cc201e3a3df35e5
bithighlander <bithighlander@gmail.com>
2024-10-22 13:46:52 -0400

    universal

--- pages/get-started.tsx
+++ pages/get-started.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
       console.log('findLatestReleaseLinks', resp.data)
       let version = resp.data.tag_name
       version = version.replace("v", "")
-      seturlMacOS("https://github.com/keepkey/keepkey-desktop/releases/download/v" + version + "/KeepKey-Desktop-" + version + "-arm64.dmg")
+      seturlMacOS("https://github.com/keepkey/keepkey-desktop/releases/download/v" + version + "/KeepKey-Desktop-" + version + "-universal.dmg")
       seturlWindows("https://github.com/keepkey/keepkey-desktop/releases/download/v" + version + "/KeepKey-Desktop-Setup-" + version + ".exe")
       seturlLinux("https://github.com/keepkey/keepkey-desktop/releases/download/v" + version + "/KeepKey-Desktop-" + version + ".AppImage")
 
